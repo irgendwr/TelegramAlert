8f495208f2e7c811996a3acf92514d263e1528b1
Jonas Bögle <jonas@boegle.me>
2022-06-18 03:47:37 +0200

    fix: Update PluginWebpackConfig to new API

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,6 @@ const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
 const path = require('path');
 
 // Remember to use the same name here and in `getUniqueId()` in the java MetaData class
-module.exports = new PluginWebpackConfig('de.irgendwr.TelegramNotificationPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
+module.exports = new PluginWebpackConfig(path.resolve(__dirname), 'de.irgendwr.TelegramNotificationPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
   // Here goes your additional webpack configuration.
 });
